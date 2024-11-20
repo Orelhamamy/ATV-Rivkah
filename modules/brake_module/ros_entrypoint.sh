0cc97a17bfe71c27abe862a839fa38a7774f7b07
Orel Hamamy Almog <orelhamamy@gmail.com>
2024-11-20 13:35:51 +0200

    Editing the DockerFile and package.xml

--- modules/brake_module/ros_entrypoint.sh
+++ modules/brake_module/ros_entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # setup ros2 environment
-source /home/ATV_ws/install/setup.bash
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
+source /root/ros2_ws/install/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 exec "$@"
